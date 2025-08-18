ccd559291d2275b7e92d3b5c6996a24443ab4ad3
mathiaslost <gmathias1717@gmail.com>
2025-08-18 20:47:21 -0300

    fix: fixing source files

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,16 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Section } from './home/entities/section.entity';
-import { Announcement } from './home/entities/announcement.entity';
-import { Contribution } from './home/entities/contribution.entity';
-import { Event } from './home/entities/event.entity';
-import { HomeModule } from './home/home.module';
 import { AnnouncementsModule } from './announcements/announcements.module';
 import { ContributionsModule } from './contributions/contributions.module';
 import { EventsModule } from './events/events.module';
-import { SectionsModule } from './sections/sections.module';
 import { HomeModule } from './home/home.module';
+import { Announcement } from './announcements/entities/announcement.entity';
+import { Contribution } from './contributions/entities/contribution.entity';
+import { Event } from './events/entities/event.entity';
 
 @Module({
   imports: [
@@ -22,15 +19,14 @@ import { HomeModule } from './home/home.module';
 		username: 'postgres',
 		password: 'postgres',
 		database: 'postgres',
-		entities: [Section, Announcement, Contribution, Event],
+		entities: [Announcement, Contribution, Event],
 		migrations: ['dist/migrations/*.js'],
 		synchronize: false, // 🚫 do not use in prod
 	}),
 	HomeModule,
 	AnnouncementsModule,
 	ContributionsModule,
-	EventsModule,
-	SectionsModule
+	EventsModule
   ],
   controllers: [AppController],
   providers: [AppService],
--- src/sections/sections.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { SectionsService } from './sections.service';
-import { SectionsController } from './sections.controller';
-
-@Module({
-  controllers: [SectionsController],
-  providers: [SectionsService],
-})
-export class SectionsModule {}
